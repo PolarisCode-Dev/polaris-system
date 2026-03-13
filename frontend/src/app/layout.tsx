eb242df34d7e9083d2ba06c64212492e04b56041
Bryachez <bryanjosueguzman3@gmail.com>
2026-03-12 23:48:54 -0600

    feat: implement RootLayout component and refactor Home page structure

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,13 @@
+import "./global.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,9 @@
+import Layout from "./layout";
+
 export default function Home() {
   return (
-    <html lang="es">
-      <head>
-        <title>My App</title>
-      </head>
-      <body>
-        <main>
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </main>
-      </body>
-    </html>
+    <Layout>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Layout>
   );
 }
