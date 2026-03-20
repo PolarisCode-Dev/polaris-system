e3d77838c6c72e5fee362daa1cae1182b021ee7f
juliovilla03 <juliofranciscovillanueva@gmail.com>
2026-03-19 23:41:29 -0600

    Error de dedo xd

--- backend/app/Http/Requests/StoreCompanyRequest.php
+++ backend/app/Http/Requests/StoreCompanyRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreComanyRequest extends FormRequest
+class StoreCompanyRequest extends FormRequest
 {
     public function authorize(): bool
     {
--- backend/app/Http/Requests/UpdateCompanyRequest.php
+++ backend/app/Http/Requests/UpdateCompanyRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateComanyRequest extends FormRequest
+class UpdateCompanyRequest extends FormRequest
 {
     public function authorize(): bool
     {
